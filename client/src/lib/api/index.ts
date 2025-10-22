0c09a5d3c282a65268ae0c99def3ce7f30446f6b
Thyago de Oliveira Perez <thyagodoliveiraperez@gmail.com>
2025-10-22 13:36:43 -0300

    feat: add token to requests

--- client/src/lib/api/index.ts
+++ client/src/lib/api/index.ts
@@ -1,10 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
+import keycloak from "../keycloak";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    if (keycloak.authenticated && keycloak.token) {
+      config.headers.Authorization = `Bearer ${keycloak.token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
